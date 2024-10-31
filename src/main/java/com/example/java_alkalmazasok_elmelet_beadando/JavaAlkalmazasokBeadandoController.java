ab27d5d0ef311a62eda65893a5343a1971e31926
Daniel Badinszky <badinszky97@gmail.com>
2024-10-31 18:06:42 +0100

    két label párhuzamos programozással változtatja az értékét

--- src/main/java/com/example/java_alkalmazasok_elmelet_beadando/JavaAlkalmazasokBeadandoController.java
+++ src/main/java/com/example/java_alkalmazasok_elmelet_beadando/JavaAlkalmazasokBeadandoController.java
@@ -1,7 +1,9 @@
 package com.example.java_alkalmazasok_elmelet_beadando;
 
-import csomag1.MNBArfolyamServiceSoap;
-import csomag1.MNBArfolyamServiceSoapImpl;
+//import csomag1.MNBArfolyamServiceSoap;
+//import csomag1.MNBArfolyamServiceSoapImpl;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,12 +12,25 @@ import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.util.Random;
 
 public class JavaAlkalmazasokBeadandoController {
 
     public MenuBar menuBar;
     @FXML
     private VBox rootVBox;
+
+
+    //---------------------------------------------
+    // Párhuzamos programozás feladatok:
+    @FXML private Label szimplaMpLabel;
+    @FXML private Label duplaMpLabel;
+    @FXML private Button randomLabelStartButton;
+    Random rand = new Random();
+    //---------------------------------------------
+
 
     private Button SoapDownload;
     
@@ -31,15 +46,15 @@ public class JavaAlkalmazasokBeadandoController {
 
     public void handleSoapDownloadClick(ActionEvent event) {
         System.out.println("Soap Letöltés start");
-        MNBArfolyamServiceSoapImpl impl = new MNBArfolyamServiceSoapImpl();
-        MNBArfolyamServiceSoap service = impl.getCustomBindingMNBArfolyamServiceSoap();
+        //MNBArfolyamServiceSoapImpl impl = new MNBArfolyamServiceSoapImpl();
+        /*MNBArfolyamServiceSoap service = impl.getCustomBindingMNBArfolyamServiceSoap();
         try {
             System.out.println(service.getInfo());
 //            System.out.println(service.getCurrentExchangeRates());
 //            System.out.println(service.getExchangeRates("2022-08-14","2022-09-14","EUR"));
         } catch (Exception e) {
             System.err.print(e);
-        }
+        }*/
     }
 
 
@@ -48,4 +63,31 @@ public class JavaAlkalmazasokBeadandoController {
 
     public void SoapGrafikon(ActionEvent actionEvent) {
     }
-}
+
+    Timeline idozito1mp = new Timeline(
+            new KeyFrame(Duration.seconds(1),
+                    e -> {
+                        int rand_int1 = rand.nextInt(10000000);
+                        szimplaMpLabel.setText(Integer.toString(rand_int1));
+                    }));
+
+    Timeline idozito2mp = new Timeline(
+            new KeyFrame(Duration.seconds(2),
+                    e -> {
+                        int rand_int1 = rand.nextInt(10000000);
+                        duplaMpLabel.setText(Integer.toString(rand_int1));
+                    }));
+
+    @FXML
+    private void startRandomLabelTexting()
+    {
+        idozito1mp.setCycleCount(Timeline.INDEFINITE);
+        idozito2mp.setCycleCount(Timeline.INDEFINITE);
+
+        idozito1mp.play();
+        idozito2mp.play();
+    }
+
+
+    }
+
